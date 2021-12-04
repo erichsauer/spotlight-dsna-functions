1c1ee6e83f1ccdd3d08d1e2689dc8061d1cbf3e6
Erich Sauer <erich.sauer@gmail.com>
2021-12-04 11:55:43 -0800

    fibonacci, now with memoization!

--- challenges/CH-fibonacci.js
+++ challenges/CH-fibonacci.js
@@ -1,7 +1,9 @@
-function fib(n)
+function fib(n, memo)
 {
+  memo = memo || {}
+  if (memo[n]) return memo[n]
   if (n < 2) return n;
-  return fib(n - 2) + fib( n - 1 )
+  return fib(n - 2, memo) + fib(n - 1, memo)
 }
 
 console.log(fib(8));
