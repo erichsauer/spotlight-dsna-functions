e79e106e673e705231586e69d404f5957a431cf8
Erich Sauer <erich.sauer@gmail.com>
2021-12-04 12:01:12 -0800

    bonus is-happy

---
+++ challenges/CH-is-happy.js
@@ -0,0 +1,15 @@
+function isHappy(n, memo) {
+  memo = memo || {}
+  const result = n
+    .toString()
+    .split("")
+    .map((num) => +num)
+    .reduce((acc, num) => (acc + num ** 2), 0);
+    console.log(result)
+  if (result === 1) return true;
+  if (memo[result]) return false;
+  memo[result] = result;
+  return isHappy(result, memo)
+}
+
+console.log(isHappy(19))
