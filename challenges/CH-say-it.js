cb0a07ffb7d3f3bff81bd3574089e71fce266d38
Erich Sauer <erich.sauer@gmail.com>
2021-12-05 17:16:39 -0800

    stumped on say-it

---
+++ challenges/CH-say-it.js
@@ -0,0 +1,13 @@
+// function sayIt(word1)
+// {
+//   return function next(word2)
+//   {
+//     return word1 + word2
+//   }
+// }
+
+// const result1 = sayIt('hello');
+// const result2 = result1('my')
+// const result = result2()
+
+// console.log(result)
